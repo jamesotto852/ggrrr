d00e43be0cfe9b1a4a679f5310e4fad085811ed9
jamesotto852 <jamesotto852@gmail.com>
2022-08-05 22:52:43 +0000

    Add examples for `_pwk()` functions

--- R/map_pwk.R
+++ R/map_pwk.R
@@ -1,3 +1,113 @@
+#' Apply a plotting function, returning a patchwork
+#'
+#' The `*_pwk()` functions apply a function `f` element-wise across various objects,
+#' converting the output into a patchwork object via `patchwork::wrap_plots()`.
+#' `f` should return a `ggplot` object, or some other object which works with patchwork
+#' (e.g. the results of `patchwork::wrap_elements()`).
+#' For details on the use of each function, see `?purrr::map`.
+#'
+#' @inheritParams purrr::map
+#' @param .patchwork_options A list of arguments passed to `patchwork::wrap_plots()`,
+#' useful for arranging plots in the resulting patchwork object. See examples for details.
+#'
+#' @return A `patchwork` object
+#'
+#' @name map_pwk
+#' @rdname map_pwk
+#'
+#' @import purrr
+#' @importFrom patchwork wrap_plots
+#'
+#' @examples
+#' library("ggplot2")
+#'
+#' # Plot different linear models w/ geom_smooth() ----
+#'
+#' df <- data.frame(x = runif(100, -10, 10))
+#' df$y <- with(df, 2 + x + .2 * x^2  - .05 * x^3+ rnorm(100, sd = 5))
+#'
+#' # plotting the data
+#' ggplot(df, aes(x, y)) +
+#'   geom_point()
+#'
+#' # f: formula -> ggplot object
+#' f <- function(formula) {
+#'   ggplot(df, aes(x, y)) +
+#'     geom_smooth(formula = formula, method = "lm")
+#' }
+#'
+#' # list of formulas to apply f over
+#' formulas <- list(
+#'   y ~ x,
+#'   y ~ poly(x, 2),
+#'   y ~ poly(x, 3)
+#' )
+#'
+#' map_pwk(formulas, f)
+#'
+#' # can specify number of columns via .patchwork_options
+#' map_pwk(formulas, f, .patchwork_options = list(ncol = 1))
+#'
+#'
+#'# Map over data.frame rows with pmap_pwk() ----
+#'
+#' geoms <- list(
+#'   geom_point(size = .5),
+#'   geom_bin2d(),
+#'   geom_density2d_filled()
+#' )
+#'
+#' datasets <- list(
+#'
+#'   # Unimodal
+#'   data.frame(
+#'     x = rnorm(4e3),
+#'     y = rnorm(4e3)
+#'   ),
+#'
+#'   # Bimodal
+#'   data.frame(
+#'     x = rnorm(4e3) + rep(c(-2, 2), length.out = 1e3),
+#'     y = rnorm(4e3)
+#'   ),
+#'
+#'   # Constrained Support
+#'   data.frame(
+#'     x = rexp(4e3),
+#'     y = rexp(4e3)
+#'   )
+#'
+#' )
+#'
+#' # plot a geom + data combination
+#' f <- function(geom, data) {
+#'   ggplot(data, aes(x, y)) +
+#'     geom
+#' }
+#'
+#' # cross product between geoms and datasets,
+#' # saved as a tibble
+#' df_nested <-
+#'   tidyr::expand_grid(
+#'     geom = geoms,
+#'     data = datasets
+#'   )
+#'
+#' # use pmap_pwk to apply across rows of tibble or data.frame
+#' # patchwork's `&` to manipulate the patchwork object
+#' pmap_pwk(df_nested, f, .patchwork_options = list(ncol = 3)) &
+#'   theme(
+#'     legend.position = "none",
+#'     axis.ticks = element_blank(),
+#'     axis.title = element_blank(),
+#'     axis.text = element_blank()
+#'   )
+#'
+NULL
+
+
+#' @rdname map_pwk
+#' @export
 map_pwk <- function(.x, .f, ..., .patchwork_options = list()) {
 
   p_list <- purrr::map(.x, .f, ...)
@@ -6,6 +116,8 @@ map_pwk <- function(.x, .f, ..., .patchwork_options = list()) {
 
 }
 
+#' @rdname map_pwk
+#' @export
 imap_pwk <- function(.x, .f, ..., .patchwork_options = list()) {
 
   p_list <- purrr::imap(.x, .f, ...)
@@ -14,6 +126,8 @@ imap_pwk <- function(.x, .f, ..., .patchwork_options = list()) {
 
 }
 
+#' @rdname map_pwk
+#' @export
 map2_pwk <- function(.x, .y, .f, ..., .patchwork_options = list()) {
 
   p_list <- purrr::map2(.x, .y, .f, ...)
@@ -22,6 +136,8 @@ map2_pwk <- function(.x, .y, .f, ..., .patchwork_options = list()) {
 
 }
 
+#' @rdname map_pwk
+#' @export
 pmap_pwk <- function(.l, .f, ..., .patchwork_options = list()) {
 
   p_list <- purrr::pmap(.l, .f, ...)
@@ -29,115 +145,6 @@ pmap_pwk <- function(.l, .f, ..., .patchwork_options = list()) {
   do.call(patchwork::wrap_plots, c(p_list, .patchwork_options))
 
 }
-
-
-
-
-
-# Examples ----------------------------------------------------------------
-library("ggplot2")
-
-p_fun <- function(scale_x = 1, scale_y = 1) {
-
-  data.frame(
-    x = 1:5 * scale_x,
-    y = 1:5 * scale_y
-  ) |>
-    ggplot(aes(x, y)) +
-    geom_point()
-
-}
-
-map_pwk(1:4, p_fun, .patchwork_options = list(ncol = 1))
-
-map2_pwk(1:4, 11:14, p_fun, .patchwork_options = list(ncol = 1))
-
-data.frame(
-  scale_x = 1:4,
-  scale_y = 11:14
-) |>
-  pmap_pwk(p_fun, .patchwork_options = list(ncol = 1))
-
-
-
-
-
-
-
-# & Example ---------------------------------------------------------------
-
-library("ggplot2"); theme_set(theme_bw())
-
-p_fun <- function(scale_x = 1, scale_y = 1) {
-
-  data.frame(
-    x = 1:5 * scale_x,
-    y = 1:5 * scale_y
-  ) |>
-    ggplot(aes(x, y)) +
-    geom_point()
-
-}
-
-p <- map_pwk(1:4, p_fun, .patchwork_options = list(ncol = 1)) &
-  theme(
-    axis.ticks = element_blank(),
-    axis.title = element_blank(),
-    axis.text = element_blank()
-  )
-
-class(p)
-
-
-
-# Cross-product of geoms and datasets -------------------------------------
-
-geoms <- list(
-  geom_point(),
-  ggdensity::geom_hdr(),
-  ggdensity::geom_hdr(method = "mvnorm")
-)
-
-datasets <- list(
-
-  # Unimodal
-  data.frame(
-    x = rnorm(1e3),
-    y = rnorm(1e3)
-  ),
-
-  # Bimodal
-  data.frame(
-    x = rnorm(1e3) + rep(c(-2, 2), length.out = 1e3),
-    y = rnorm(1e3)
-  ),
-
-  # Constrained Support
-  data.frame(
-    x = rexp(1e3),
-    y = rexp(1e3)
-  )
-
-)
-
-p_fun <- function(geom, data) {
-  ggplot(data, aes(x, y)) +
-    geom
-}
-
-
-tidyr::expand_grid(
-  geom = geoms,
-  data = datasets
-) |>
-  pmap_pwk(p_fun, .patchwork_options = list(ncol = 3)) &
-  theme(
-    legend.position = "none",
-    axis.ticks = element_blank(),
-    axis.title = element_blank(),
-    axis.text = element_blank()
-  )
-
 
 
 
